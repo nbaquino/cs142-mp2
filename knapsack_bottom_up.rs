503e85bca5feb2d2a304ba46d7933dbef55c3b08
nbaquino <nbaquino02@gmail.com>
2024-12-23 22:32:22 +0800

    fix knapsack out out bounds

--- knapsack_bottom_up.rs
+++ knapsack_bottom_up.rs
@@ -1,19 +1,23 @@
 use std::cmp::max;
 
 pub fn knapsack_bottom_up(w: usize, weights: &Vec<usize>, profits: &Vec<usize>, n: usize, dp: &mut Vec<Vec<usize>>) -> usize {
-    for i in 0..n + 1 {
+    // Initialize the first column to 0
+    for i in 0..=n {
         dp[i][0] = 0;
     }
-    for i in 0..w + 1 {
-        dp[0][i] = 0;
+    // Initialize the first row to 0
+    for j in 0..=w {
+        dp[0][j] = 0;
     }
-    for i in 1..n + 1 {
-        for j in 1..weights[i - 1] {
-            dp[i][j] = dp[i - 1][j];
-        }
-        for j in weights[i - 1]..w + 1 {
-            dp[i][j] = max(dp[i - 1][j], profits[i - 1] + dp[i - 1][j - weights[i - 1]]);
+
+    for i in 1..=n {
+        for j in 1..=w { // Iterate from 1 to w
+            if j >= weights[i - 1] { // Check if the current weight can be included
+                dp[i][j] = max(dp[i - 1][j], profits[i - 1] + dp[i - 1][j - weights[i - 1]]);
+            } else {
+                dp[i][j] = dp[i - 1][j]; // If the weight can't be included, carry forward the previous value
+            }
         }
     }
-    dp[n][w]
+    dp[n][w] // Return the maximum profit for n items and capacity w
 }
