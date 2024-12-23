c2d12d4b3dbf205ce547497f8d1b1ba9dbbe7870
nbaquino <nbaquino02@gmail.com>
2024-12-23 22:58:11 +0800

    added table borders and average time and value

--- main.rs
+++ main.rs
@@ -18,22 +18,35 @@ fn main() {
 
     let test_sizes = vec![100, 1000, 5000, 10000, 25000, 50000, 75000, 100000];
 
-    for &n in &test_sizes {
-        println!("Testing for n = {}", n);
+    // Print table header
+    println!("----------------------------------------------------");
+    println!("0/1 Knapsack Problem using Bottom Up DP (Tabulation)");
+    println!("----------------------------------------------------");
+    println!("N\tRun\tValue\tTime(s)");
 
+    for &n in &test_sizes {
         let mut times = vec![];
-        for _ in 0..3 { // Run 3 times for averaging
+        let mut total_value = 0;
+        for run in 1..=3 { // Run 3 times for averaging
             let (weights, profits) = generate_data(n, weight_range, profit_range);
             let mut dp = vec![vec![0; capacity + 1]; n + 1];
 
             let start = Instant::now();
-            knapsack_bottom_up(capacity, &weights, &profits, n, &mut dp);
+            let value = knapsack_bottom_up(capacity, &weights, &profits, n, &mut dp);
             let duration = start.elapsed();
 
-            times.push(duration.as_secs_f64());
+            let time_taken = duration.as_secs_f64();
+            times.push(time_taken);
+            total_value += value;
+            // Print each run's results
+            println!("{}\t{}\t{}\t{:.6}", n, run, value, time_taken);
         }
 
+        let avg_value = total_value as f64 / times.len() as f64;
         let avg_time: f64 = times.iter().sum::<f64>() / times.len() as f64;
-        println!("n = {}, Average Time = {:.6} seconds", n, avg_time);
+        // Print average results with borders
+        println!("----------------------------------------------------");
+        println!("\tAverage\t\t{:.6}\t{:.6}", avg_value, avg_time);
+        println!("----------------------------------------------------");
     }
 }
